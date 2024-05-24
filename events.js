28fa2f4857f196efd83240f1439cfe38faba96fd
Belkiss-Hammami <155115126+Belkiss-Hammami@users.noreply.github.com>
2024-05-24 10:37:11 +0100

    Add files via upload

---
+++ events.js
@@ -0,0 +1,82 @@
+const eventsValidation=require("../validation/Events");
+const eventSchema=require("../models/eventSchema");
+const { func } = require("joi");
+//createEvent getallevent getonevent delete put
+
+
+let getAllEvents=async(req,res)=>{
+    try{
+        let events=await eventSchema.find();
+        res.status(200).json(events);
+    }
+    catch(e){
+        console.log(e);
+        res.status(500).json({message:"error not found"});
+    }
+    
+}
+let getOneeEvent=async(req,res)=>{
+    let eventId =req.params.eventId
+    try{
+        let event=await eventSchema.findById(eventId);
+        res.status(200).json(event);
+
+    }
+    catch(e){
+        return res.status(500).json({message:"this id does not exist please try again"});
+    }
+
+}
+
+
+let createEvent=async (req,res)=>{
+    //validation part 
+    let {error,value } = eventsValidation.validate(req.body);
+    if (error){
+        return res.status(400).json({message : error.details[0].message})
+    }
+    //adding event
+    try{
+        let newevent = await eventSchema.create(req.body)
+        res.status(201).json(newevent)
+
+    }catch(e){
+        console.log(e)
+        res.status(500).json({message:"error while creating new event"})
+    }
+    
+}
+let deleteEvent = async (req,res)=>{
+    let eventId =req.params.id
+    try{
+        let event=await eventSchema.findByIdAndDelete(eventId);
+        res.status(200).json({message:"deleted successfully"});
+    }
+    catch(e){
+        return res.status(500).json({message:"this id does not exist please try again"});
+    }
+}
+let updateEvent= async (req,res) => {
+    const eventId =req.params.id;
+    let {error, value}= eventsValidation.validate(req.body)
+    if (error){
+    return res.status(400).json({message : error.details[0].message})
+    }
+    try{
+        let event= await eventSchema.findByIdAndUpdate(eventId,value,{new:true,old:false});
+        res.status(200).json({message:"updated successfully"})
+        
+    }
+    catch(e){
+        return res.status(404).json({message:"this id does not exist please try again"});
+    }
+}
+module.exports={
+
+    getAllEvents,
+    getOneeEvent,
+    createEvent,
+    deleteEvent,
+    updateEvent
+}
+
